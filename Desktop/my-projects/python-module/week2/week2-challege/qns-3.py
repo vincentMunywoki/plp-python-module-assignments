d71c851062f029fc1626cdbc37dc57048339c840
vincent munywoki <vincentmunywoki12@gmail.com>
2024-11-11 12:04:03 +0300

    qns-3.PY

--- Desktop/my-projects/python-module/week2/week2-challege/qns-3.py
+++ Desktop/my-projects/python-module/week2/week2-challege/qns-3.py
@@ -1,1 +1,21 @@
-# Write a program that uses a dictionary to store information about a person, such as their name, age, and favorite color. Ask the user for input and store the information in the dictionary. Then, print the dictionary to the console.
+# Write a program that uses a dictionary to store information about a person, 
+# such as their name, age, and favorite color. Ask the user for input and store the information in the dictionary.
+#  Then, print the dictionary to the console.# Create an empty dictionary to store user information
+
+
+person_info = {}
+
+# Ask the user for input and store it in the dictionary
+person_info["name"] = input("Enter your name: ")
+person_info["age"] = input("Enter your age: ")
+person_info["favorite_color"] = input("Enter your favorite color: ")
+
+# Print the dictionary
+print(person_info)
+
+my_name = {}
+
+my_name["school"] = input("My school: ")
+my_name["course"] = input("my course is: ")
+
+print(my_name)
