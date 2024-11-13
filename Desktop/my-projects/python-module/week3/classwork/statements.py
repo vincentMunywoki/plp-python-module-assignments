78c286d6b6f77cdcf40b019d8c103154ea3dd22b
vincent munywoki <vincentmunywoki12@gmail.com>
2024-11-13 10:36:21 +0300

    if statements

---
+++ Desktop/my-projects/python-module/week3/classwork/statements.py
@@ -0,0 +1,28 @@
+# a =33
+# b =296
+
+# if a > b:
+#     print("A is greater than B")
+
+
+# a= 33
+# b=296
+
+# if a> b:
+#     print("A is greater than B")
+# else: 
+#     print("B is greater than A")
+
+
+
+a = 200
+b = 33
+
+if b > a:
+    print("B is greter than A")
+
+elif a==b:
+    print("A and B are equal values")
+
+else:
+    print("A is greter than B")
