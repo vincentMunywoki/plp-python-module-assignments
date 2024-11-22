9b3fe354cd76bf006fe7025509d295288df4291e
vincent munywoki <vincentmunywoki12@gmail.com>
2024-11-22 13:44:27 +0300

    class update

--- Desktop/my-projects/python-module/week5/assignment/class.py
+++ Desktop/my-projects/python-module/week5/assignment/class.py
@@ -1,8 +1,8 @@
 class Superhero:
     def __init__(self, name, power, alias):
-        self.name = name      # Real name of the superhero
-        self.power = power    # The superpower they possess
-        self.alias = alias    # Superhero alias (e.g., "Spider-Man")
+        self.name = name      # name of the superhero
+        self.power = power    # superpower they possess
+        self.alias = alias    # Superhero alias 
     
     def introduce(self):
         return f"Hi, I am {self.alias}, but my real name is {self.name}. I have the power of {self.power}!"
@@ -13,7 +13,7 @@ class Superhero:
     def __str__(self):
         return f"Superhero: {self.alias}, Real Name: {self.name}, Power: {self.power}"
 
-# Creating an instance of Superhero
+# Lets create an instance of Superhero
 superhero = Superhero("Peter Parker", "Web-Slinging", "Spider-Man")
 print(superhero.introduce())
 print(superhero.use_power())
