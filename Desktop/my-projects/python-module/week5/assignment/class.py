a3714b37b2b14069b917dd1ff879506eba2779d4
vincent munywoki <vincentmunywoki12@gmail.com>
2024-11-22 13:48:46 +0300

    class update

--- Desktop/my-projects/python-module/week5/assignment/class.py
+++ Desktop/my-projects/python-module/week5/assignment/class.py
@@ -17,3 +17,20 @@ class Superhero:
 superhero = Superhero("Peter Parker", "Web-Slinging", "Spider-Man")
 print(superhero.introduce())
 print(superhero.use_power())
+
+class Villain(Superhero):
+    def __init__(self, name, power, alias, evil_plan):
+        super().__init__(name, power, alias)
+        self.evil_plan = evil_plan
+    
+    def reveal_plan(self):
+        return f"{self.alias}'s evil plan is: {self.evil_plan}"
+
+    def use_power(self):
+        return f"{self.alias} is using {self.power} to cause chaos!"
+
+# Creating an instance of Villain
+villain = Villain("Norman Osborn", "Super Strength", "Green Goblin", "Take over the city!")
+print(villain.reveal_plan())
+print(villain.use_power())
+
