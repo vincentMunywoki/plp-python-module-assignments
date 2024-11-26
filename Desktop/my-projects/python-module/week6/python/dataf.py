0ead8a782f51809fd9db39e210e935c7dcee3b54
vincent munywoki <vincentmunywoki12@gmail.com>
2024-11-26 17:48:41 +0300

    week 6 dataframe

--- Desktop/my-projects/python-module/week6/python/dataf.py
+++ Desktop/my-projects/python-module/week6/python/dataf.py
@@ -54,15 +54,4 @@ data = {
 df = pd.DataFrame(data)
 
 print(df)
-import pandas as pd
 
-# Creating a DataFrame from a dictionary
-data = {
-    'Name': ['Alice', 'Bob', 'Charlie'],
-    'Age': [25, 30, 35],
-    'City': ['New York', 'Los Angeles', 'Chicago']
-}
-
-df = pd.DataFrame(data)
-
-print(df)
